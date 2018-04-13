6f8434d915286c4b7061c717a1d4f3e53aa8d825
gputintsev <g.putintsev@qiwi.ru>
2018-04-13 16:41:10 +0300

    working component script inject

--- src/app/componenets.tsx
+++ src/app/componenets.tsx
@@ -13,14 +13,19 @@ export module Componenets {
     }
 
     export class ContentEntry extends React.Component<{key:number, data:Entity.Content}, {}>{
+        instance
+        componentDidMount() {
+            const s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = 'https://telegram.org/js/telegram-widget.js?4s';
+            s.setAttribute("data-telegram-post", "radio_persimmon/" +  this.props.data.originalId)
+            this.instance.appendChild(s);
+          }
         
-        render(){
-            return(
-                <div>
-                    <script async src="https://telegram.org/js/telegram-widget.js?4" data-telegram-post={"radio_persimmon/" + this.props.data.originalId} data-width="100%"/>
-                </div>
-            )
-        }
+          render() {
+            return <div ref={el => (this.instance = el)} />;
+          }
     }
 
     
